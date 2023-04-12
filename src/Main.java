dd55eafc3cbbd8aac1a8b286d9ceff91c37b817a
smithbwlhs <smithb@wlwv.k12.or.us>
2023-04-11 22:29:09 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(5);
+        //Rectangle rectangle = new Rectangle(5, 7);
+
+        // The Square toString will override the rectangle toString
+        //System.out.println(square);
+
+        // The Rectangle will return the toString from the Rectangle class
+        //System.out.println(rectangle);
+    }
+}
